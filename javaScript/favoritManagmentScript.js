5a914c058486276675cfda1cdd435994416711e5
DisasterUnknown <priyashanthafdo26@gmail.com>
2024-07-13 19:01:34 +0530

    ajusted some validations in the js

--- javaScript/favoritManagmentScript.js
+++ javaScript/favoritManagmentScript.js
@@ -9,21 +9,27 @@ console.log("Wolf");
 // Adding the current item list to the favorits list 
 function addFavorit() {
 
-    let tableData = [];
-    for (let i = 1; i < (cartTable.rows.length - 2); i++) {
-        let row = cartTable.rows[i];
+    if (cartTable.rows.length > 3) {
+        let tableData = [];
+        for (let i = 1; i < (cartTable.rows.length - 2); i++) {
+            let row = cartTable.rows[i];
 
-        let item = row.cells[0].textContent;
-        let quantity = row.cells[1].textContent;
-        let price = parseInt((row.cells[2].textContent).replace(/,/g, ""));
-        // console.log(`Item: ${item}, Quantity: ${quantity}, Price: ${price}`);
+            let item = row.cells[0].textContent;
+            let quantity = row.cells[1].textContent;
+            let price = parseInt((row.cells[2].textContent).replace(/,/g, ""));
+            // console.log(`Item: ${item}, Quantity: ${quantity}, Price: ${price}`);
 
-        tableData.push({ item, quantity, price });
+            tableData.push({ item, quantity, price });
+        }
+
+        // Converting the data into JSON string
+        let tableDataJSON = JSON.stringify(tableData);
+        localStorage.setItem("favoritList", tableDataJSON);
+
+        window.alert("Data added");
+    } else {
+        window.alert("The Cart is Empty!");
     }
-
-    // Converting the data into JSON string
-    let tableDataJSON = JSON.stringify(tableData);
-    localStorage.setItem("favoritList", tableDataJSON);
 }
 
 
@@ -33,12 +39,12 @@ function useFavorit() {
 
     if (!tableDataJSON) {
         window.alert("No Data Found!!");
-    }
+    } else {
 
-    // Converting the data into complex data 
-    let tableData = JSON.parse(tableDataJSON);
+        // Converting the data into complex data 
+        let tableData = JSON.parse(tableDataJSON);
 
-    cartTable.innerHTML = `
+        cartTable.innerHTML = `
         <tr>
             <th>Item</th>
             <th>Quantity</th>
@@ -46,26 +52,26 @@ function useFavorit() {
         </tr>
     `;
 
-    let cartTotal = 0;
-    tableData.forEach(data => {
-        let row = cartTable.insertRow();
+        let cartTotal = 0;
+        tableData.forEach(data => {
+            let row = cartTable.insertRow();
 
-        let itemCell1 = row.insertCell(0);
-        let itemCell2 = row.insertCell(1);
-        let itemCell3 = row.insertCell(2);
+            let itemCell1 = row.insertCell(0);
+            let itemCell2 = row.insertCell(1);
+            let itemCell3 = row.insertCell(2);
 
-        itemCell1.textContent = data.item;
-        itemCell1.className = "items";
-        itemCell2.textContent = data.quantity;
-        itemCell3.innerHTML = `<div class="priceCell">
+            itemCell1.textContent = data.item;
+            itemCell1.className = "items";
+            itemCell2.textContent = data.quantity;
+            itemCell3.innerHTML = `<div class="priceCell">
                                             <span>${data.price.toLocaleString('en-US')}</span>
                                             <button onclick="deleteRow(this)">x</button>
                                         </div>`;
 
-        cartTotal += data.price;
-    });
+            cartTotal += data.price;
+        });
 
-    cartTable.innerHTML += `
+        cartTable.innerHTML += `
     <tr>
         <td class="items">&nbsp;</td>
         <td class="quantity">&nbsp;</td>
@@ -77,6 +83,7 @@ function useFavorit() {
         <td id="totalPrice">${cartTotal.toLocaleString('en-US')}</td>
     </tr>
     `
+    }
 }
 
 
@@ -92,7 +99,7 @@ function changePage() {
             let quantity = row.cells[1].textContent;
             let price = parseInt((row.cells[2].textContent).replace(/,/g, ''));
 
-            currentTableData.push({item, quantity, price});
+            currentTableData.push({ item, quantity, price });
         }
 
         let currentTableDataJSON = JSON.stringify(currentTableData);
